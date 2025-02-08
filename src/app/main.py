f9deb76c8b8ba0c0de8f155a05ee940fde09da30
ismailwedihaji <ismailwedihaji@gmail.com>
2025-02-09 00:46:20 +0100

    tes

--- src/app/main.py
+++
@@ -1,9 +0,0 @@
-from CIServer import SimpleHandler
-from http.server import HTTPServer, BaseHTTPRequestHandler
-from CIServer import run_server
-
-
-server = run_server(8008)
-server.serve_forever()
-
-
